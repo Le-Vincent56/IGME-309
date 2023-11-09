751db1bcb64968f5ec91ebb8bc507973830d4a82
Le-Vincent56 <le.vincent56@gmail.com>
2023-11-09 17:36:42 -0500

    Corrected HW3

--- HW03 - Particle System/HW03 - Particle System/ParticleSystem.cpp
+++ HW03 - Particle System/HW03 - Particle System/ParticleSystem.cpp
@@ -22,20 +22,20 @@ ParticleSystem::ParticleSystem(int _numParticles)
 		// Please add initializations for other arrays as you see appropriate.
 		
 		// Positions initialization
-		positions[i] = 0;
-		positions[i * 2] = 0;
-		positions[i * 3] = 0;
+		positions[i * 3 + 0] = 0;
+		positions[i * 3 + 1] = 0;
+		positions[i * 3 + 2] = 0;
 
 		// Velocity initialization
-		velocities[i] = getRandomValue(minSpeedX, maxSpeedX);
-		velocities[i * 2] = getRandomValue(minSpeedY, maxSpeedY);
-		velocities[i * 3] = getRandomValue(minSpeedZ, maxSpeedZ);
+		velocities[i * 3 + 0] = getRandomValue(minSpeedX, maxSpeedX);
+		velocities[i * 3 + 1] = getRandomValue(minSpeedY, maxSpeedY);
+		velocities[i * 3 + 2] = getRandomValue(minSpeedZ, maxSpeedZ);
 
 		// Colors initialization
-		colors[i] = 0;
-		colors[i * 2] = 0;
-		colors[i * 3] = 255;
-		colors[i * 4] = 255;
+		colors[i * 4 + 0] = 0;
+		colors[i * 4 + 1] = 0;
+		colors[i * 4 + 2] = 255;
+		colors[i * 4 + 3] = 255;
 
 		/***************************/
 	}
@@ -58,31 +58,31 @@ void ParticleSystem::update(float deltaTime)
 			lifeTimes[i] = maxLifeTime - maxLifeTime * i / numParticles;
 
 			// Positions initialization
-			positions[i] = 0;
-			positions[i * 2] = 0;
-			positions[i * 3] = 0;
+			positions[i * 3 + 0] = 0;
+			positions[i * 3 + 1] = 0;
+			positions[i * 3 + 2] = 0;
 
 			// Velocity initialization
-			velocities[i] = getRandomValue(minSpeedX, maxSpeedX);
-			velocities[i * 2] = getRandomValue(minSpeedY, maxSpeedY);
-			velocities[i * 3] = getRandomValue(minSpeedZ, maxSpeedZ);
+			velocities[i * 3 + 0] = getRandomValue(minSpeedX, maxSpeedX);
+			velocities[i * 3 + 1] = getRandomValue(minSpeedY, maxSpeedY);
+			velocities[i * 3 + 2] = getRandomValue(minSpeedZ, maxSpeedZ);
 
 			// Colors initialization
-			colors[i] = 0;
-			colors[i * 2] = 0;
-			colors[i * 3] = 255;
-			colors[i * 4] = 255;
+			colors[i * 4 + 0] = 0;
+			colors[i * 4 + 1] = 0;
+			colors[i * 4 + 2] = 255;
+			colors[i * 4 + 3] = 255;
 		}
 
 		// Change velocity based on acceleration and deltaTime
-		velocities[i] += acceleration[0] * deltaTime;
-		velocities[i * 2] += acceleration[1] * deltaTime;
-		velocities[i * 3] += acceleration[2] * deltaTime;
+		velocities[i * 3 + 0] += acceleration[0] * deltaTime;
+		velocities[i * 3 + 1] += acceleration[1] * deltaTime;
+		velocities[i * 3 + 2] += acceleration[2] * deltaTime;
 
 		// Update positions based on velocities
-		positions[i] += velocities[i] * deltaTime;
-		positions[i * 2] += velocities[i * 2] * deltaTime;
-		positions[i * 3] += velocities[i * 3] * deltaTime;
+		positions[i * 3 + 0] += velocities[i * 3 + 0] * deltaTime;
+		positions[i * 3 + 1] += velocities[i * 3 + 1] * deltaTime;
+		positions[i * 3 + 2] += velocities[i * 3 + 2] * deltaTime;
 
 		// Update alpha channel based on the ratio of the lifeTime to maxLifeTime
 		colors[i * 4] *= (lifeTimes[i] / maxLifeTime);
@@ -102,10 +102,10 @@ void ParticleSystem::draw()
 	glBegin(GL_POINTS);
 	for (int i = 0; i < numParticles; i++) {
 		// Change colors
-		glColor4f(colors[i], colors[i * 2], colors[i * 3], colors[i * 4]);
+		glColor4f(colors[i * 4 + 0], colors[i * 4 * 1], colors[i * 4 + 2], colors[i * 4 + 3]);
 
 		// Draw positions
-		glVertex3f(positions[i], positions[i * 2], positions[i * 3]);
+		glVertex3f(positions[i * 3 + 0], positions[i * 3 + 1], positions[i * 3 + 2]);
 	}
 	glEnd();
 
